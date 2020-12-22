19f1eb6f43128d990c64a43209e5b722660ce5dc
Shelly Nixon <shellymnixon@gmail.com>
2020-12-21 21:42:48 -0500

    update

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -26,57 +26,42 @@ function buildtable(sampleID){
 function buildchart(sampleID){
     d3.json("samples.json").then((sampledata)=>{
         var samples = sampledata.samples
-       
         var filterdata = samples.filter(x => x.id == sampleID)
-        
         var x_axis = filterdata[0].sample_values.slice(0,10)
-        for (x=0; x<10; x++){
-            var y_axis = filterdata[0].otu_ids[x]
-            var labels = filterdata[0].otu_labels.slice[x]};
+        var y_axis = filterdata[0].otu_ids[0]
+        var labels = filterdata[0].otu_labels[0]
         var trace1 = {
             x: x_axis,
             y: y_axis,
             text: labels,
             type: "bar",
-            orientation: "h"
-        };
-
+            orientation: "h"};
         var data = [trace1];
-
         var layout = {
-            title: "Top 10 OTUs Found",}
-
+            title: "Top 10 OTUs Found",
+        };
         Plotly.newPlot("bar", data, layout);
-
-        var x_data = filterdata[0].otu_ids;
-        var y_data = filterdata[0].sample_values;
-        var o_labels = filterdata[0].otu_labels;
-
+        var x_data = filterdata[0].otu_ids
+        var y_data = filterdata[0].sample_values
+        var o_labels = filterdata[0].otu_labels
         var trace2 = {
             x: x_data,
             y: y_data,
+            text: o_labels,
             mode: "markers",
             marker:{
                 size: filterdata[0].sample_values,
                 color: filterdata[0].otu_ids}
-
         };
-
         var layout1 = {
             title: "Sample Display",
             xaxis: {
-                title: filterdata[0].id
-            }   
+                title: filterdata[0].id}   
         }
-
         var data1 = [trace2];   
-
         Plotly.newPlot("bubble", data1, layout1);
     })
 };
-
-
-
 function optionChanged(newsample){
     buildtable(newsample)
     buildchart(newsample)
